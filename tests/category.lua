e60cde5911dcf3574bb1bc0733ce8d1def193271
Nathan Hartland <n.p.hartland@vu.nl>
2018-01-22 01:29:11 +0100

    category test examples

--- tests/category.lua
+++ tests/category.lua
@@ -1,17 +1,18 @@
---- Tests of basic forma point functions
+--- Tests of basic forma category functions
 require 'busted.runner'()
 local neighbourhood = require("neighbourhood")
 local categories    = require("categories")
 
+-- There should be 2^n categories for a neighbourhood with n elements
 describe("category test", function()
-    test("forma.categories.generate (moore)", function()
-        local moore            = neighbourhood.moore()
-        local moore_categories = categories.generate(moore)
-        assert.are_equals(256, #moore_categories)
-    end)
     test("forma.categories.generate (von neumann)", function()
         local von_neumann    = neighbourhood.von_neumann()
         local vn_categories  = categories.generate(von_neumann)
         assert.are_equals(16, #vn_categories )
+    end)
+    test("forma.categories.generate (moore)", function()
+        local moore            = neighbourhood.moore()
+        local moore_categories = categories.generate(moore)
+        assert.are_equals(256, #moore_categories)
     end)
 end)
