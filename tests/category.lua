3db3a2014b1e59ef3d0672281461afd6c95da5e4
Nathan Hartland <n.p.hartland@vu.nl>
2018-01-22 01:27:08 +0100

    Neighbourhood categories

---
+++ tests/category.lua
@@ -0,0 +1,17 @@
+--- Tests of basic forma point functions
+require 'busted.runner'()
+local neighbourhood = require("neighbourhood")
+local categories    = require("categories")
+
+describe("category test", function()
+    test("forma.categories.generate (moore)", function()
+        local moore            = neighbourhood.moore()
+        local moore_categories = categories.generate(moore)
+        assert.are_equals(256, #moore_categories)
+    end)
+    test("forma.categories.generate (von neumann)", function()
+        local von_neumann    = neighbourhood.von_neumann()
+        local vn_categories  = categories.generate(von_neumann)
+        assert.are_equals(16, #vn_categories )
+    end)
+end)
--- util.lua
+++ util.lua
@@ -1,13 +1,14 @@
--- utility.lua
 --- Utilities
+-- @module forma.util
 
-local utl = {}
+local util = {}
+local pattern = require('pattern')
 
 --- C++ style pop-and-swap for unordered lists.
 -- @param lst input list.
 -- @param t target to be removed from list.
 -- @return true if target is found and removed, false if not
-function utl.popandswap(lst, t)
+function util.popandswap(lst, t)
 	for i=1,#lst,1 do
 	    if lst[i] == t then
 	        lst[i] = lst[#lst]
@@ -18,17 +19,22 @@ function utl.popandswap(lst, t)
 	return false
 end
 
--- Returns true if element e is present in list t, false otherwise
-function utl.intable(e, t)
+--- Method for checking if an element is in a list.
+-- @param e element to check for in list.
+-- @param t input list.
+-- @return true if e is present in t, false if not
+function util.intable(e, t)
     assert(type(t) == 'table')
     for i=1,#t,1 do
         if e == t[i] then return true end
     end return false
 end
 
--- Fisher-Yates shuffle
--- Returns a shuffled version of the input table
-function utl.fisher_yates(table, rng)
+--- Fisher-Yates shuffle.
+-- Performs an in-place shuffle of the input table
+-- @param table the table to be shuffled.
+-- @param rng a random number generator (syntax as per math.random).
+function util.fisher_yates(table, rng)
   if rng == nil then rng = math.random end
   for i=#table,1,-1 do
     local j = rng(#table)
@@ -36,4 +42,25 @@ function utl.fisher_yates(table, rng)
   end
 end
 
-return utl
+--- Pretty print a list of forma.patterns.
+-- Prints a list of pattern segments to the terminal, with a given list of
+-- chars per segment.
+-- @param domain the basic patterns from which the segments are drawn.
+-- @param segments the table of segments to be drawn.
+-- @param chars the characters to be printed for each segment.
+function util.pretty_print(domain, segments, chars)
+    print('$')
+    -- Print out the segments to a map
+    for i=domain.min.y, domain.max.y,1 do
+        local string = '.'
+        for j=domain.min.x, domain.max.x,1 do
+            local token = ' '
+            for k,v in ipairs(segments) do
+            if pattern.point(v, j, i) ~= nil then token = chars[k] end end
+            string = string .. token
+        end 
+        print(string)
+    end
+end
+
+return util
