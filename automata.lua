26835c9c56c98eda46aaf13d3512bb61df14da95
Nathan Hartland <n.p.hartland@vu.nl>
2018-05-31 01:24:29 +0200

    Decoupling automata from pointset

--- automata.lua
+++ automata.lua
@@ -14,15 +14,17 @@ local util = require(thispath .. 'util')
 -- @param ruleset a list of forma.rules for performing the CA on
 -- @return the next iteration, and a bool specifying if convergence has been reached.
 function automata.iterate(prevp, domain, ruleset)
-	assert(getmetatable(prevp) == pattern,  "forma.automata: iterate requires a pattern as a first argument")
-	assert(getmetatable(domain) == pattern, "forma.automata: iterate requires a pattern as a second argument")
+	assert(getmetatable(prevp) == pattern,
+           "forma.automata: iterate requires a pattern as a first argument")
+	assert(getmetatable(domain) == pattern,
+           "forma.automata: iterate requires a pattern as a second argument")
 	local nextp = pattern.new()
     for i=1, #domain.pointset, 1 do
         local v = domain.pointset[i]
         local alive_cell = rule.check(ruleset, prevp, v)
-		if alive_cell == true then pattern.insert(nextp, v.x, v.y) end
+		if alive_cell == true then nextp:insert(v.x, v.y) end
 	end
-	local converged = (pattern.size(nextp) == pattern.size(prevp)) and pattern.size(nextp-prevp) == 0
+	local converged = (nextp:size() == prevp:size()) and (nextp-prevp):size() == 0
 	return nextp, converged
 end
 
@@ -35,15 +37,17 @@ end
 -- @param rng a (optional) random number generator (syntax as per math.random).
 -- @return the next iteration, and a bool specifying if convergence has been reached.
 function automata.grow(prevp, domain, ruleset, rng)
-	assert(getmetatable(prevp)  == pattern, "forma.automata: iterate requires a pattern as a first argument")
-	assert(getmetatable(domain) == pattern, "forma.automata: iterate requires a pattern as a second argument")
-    local testdomain = pattern.intersection(pattern.edge(prevp), domain)
-    local testpoints = testdomain.pointset
+	assert(getmetatable(prevp)  == pattern,
+           "forma.automata: iterate requires a pattern as a first argument")
+	assert(getmetatable(domain) == pattern,
+           "forma.automata: iterate requires a pattern as a second argument")
+    local testdomain = pattern.intersection(prevp:edge(), domain)
+    local testpoints = testdomain:pointlist()
     util.fisher_yates(testpoints, rng)
     for i=1, #testpoints, 1 do
        if rule.check(ruleset, prevp, testpoints[i]) == true then
            local nextp = pattern.clone(prevp)
-           pattern.insert(nextp, testpoints[i].x, testpoints[i].y)
+           nextp:insert(testpoints[i].x, testpoints[i].y)
 	       return nextp, false
        end
     end
