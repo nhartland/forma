f98392bd9ed742df7b4ef55ca0a48caea86bc68f
Nathan Hartland <n.p.hartland@vu.nl>
2018-06-07 01:19:10 +0200

    Make docs/readme/img source of example images

--- generate_img.sh
+++ generate_img.sh
@@ -5,22 +5,24 @@
 #  - imagemagick
 #  - Input Mono Light font
 
+IMG_TARGET=./docs/readme/img
+
 mkdir tmp
 
 luajit examples/carpet.lua > tmp/header.txt
 convert -size 650x320 xc:black +antialias -font "InputMonoL" -pointsize 12 -fill white \
--annotate +5+12 "@tmp/header.txt" img/header.png
+-annotate +5+12 "@tmp/header.txt" $IMG_TARGET/header.png
 
 luajit examples/game_of_life_async.lua > tmp/symmetry.txt
 convert -size 650x320 xc:black +antialias -font "InputMonoL" -pointsize 12 -fill white \
--annotate +5+12 "@tmp/symmetry.txt" img/symmetry.png
+-annotate +5+12 "@tmp/symmetry.txt" $IMG_TARGET/symmetry.png
 
 luajit examples/caves.lua > tmp/caves.txt
 convert -size 650x320 xc:black +antialias -font "InputMonoL" -pointsize 12 -fill white \
--annotate +5+12 "@tmp/caves.txt" img/caves.png
+-annotate +5+12 "@tmp/caves.txt" $IMG_TARGET/caves.png
 
 luajit examples/corridors.lua > tmp/corridor.txt
 convert -size 650x320 xc:black +antialias -font "InputMonoL" -pointsize 12 -fill white \
--annotate +5+12 "@tmp/corridor.txt" img/corridor.png
+-annotate +5+12 "@tmp/corridor.txt" $IMG_TARGET/corridor.png
 
 rm -rf tmp
