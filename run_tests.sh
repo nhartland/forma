1f06b1eea5bd8e0a2bfe87287298705b9d6fe2e7
Nathan Hartland <n.p.hartland@vu.nl>
2018-06-23 18:23:00 +0200

    Travis first attempt

--- run_tests.sh
+++ run_tests.sh
@@ -3,20 +3,27 @@
 # Requirements:
 #       luaunit
 #       luacov
-LUA=luajit
 
-echo "Running luaunit tests"
+LUA=${LUA:=luajit}
+
+# Run luaunit tests
 $LUA -lluacov tests/run.lua -o TAP
-echo "Generating luacov report"
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+# Generate luacov report
 luacov && rm luacov.stats.out 
 
 echo "Running examples"
 for example in ./examples/*.lua; do
     $LUA "$example" > /dev/null
-    RETVAL=$?
-    if [ $RETVAL -ne 0 ]; then
+    if [ $? -ne 0 ]; then
         echo "$example fails"
+        exit 1
     else
         echo "$example passes"
     fi
 done
+
+exit 0
