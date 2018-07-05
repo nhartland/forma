4e3742c207709623687b11aeacc25e2709ff4587
Nathan Hartland <n.p.hartland@vu.nl>
2018-07-06 00:52:51 +0200

    Coveralls (#6)
    Introduce coveralls

--- run_tests.sh
+++ run_tests.sh
@@ -7,13 +7,10 @@
 LUA=${LUA:=luajit}
 
 # Run luaunit tests
-$LUA -lluacov tests/run.lua -o TAP
+$LUA tests/run.lua -o TAP
 if [ $? -ne 0 ]; then
     exit $?
 fi
-
-# Generate luacov report
-luacov && rm luacov.stats.out 
 
 echo "Running examples"
 for example in ./examples/*.lua; do
