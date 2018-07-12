c51e08077902aba774df967361cf0666fd30c575
Nathan Hartland <n.p.hartland@vu.nl>
2018-07-13 01:21:49 +0200

    Tests for square and line primitives

--- tests/primitives.lua
+++ tests/primitives.lua
@@ -1,13 +1,11 @@
 --- Tests of basic forma primitives
 local lu = require('luaunit')
 local cell       = require("forma.cell")
+local pattern    = require("forma.pattern")
 local primitives = require("forma.primitives")
 local subpattern = require("forma.subpattern")
 
 testPrimitives = {}
-
-function testPrimitives:setUp()
-end
 
 -- Test circle raster ------------------------------------------------
 function testPrimitives:testCircle()
@@ -19,6 +17,31 @@ function testPrimitives:testCircle()
         for pt in inside_circle[1]:cells() do
             lu.assertTrue(cell.euclidean(pt, cell.new(0,0)) < i )
         end
+    end
+end
+-- Test square raster ------------------------------------------------
+function testPrimitives:testCircle()
+    for i=1,10,1 do
+        -- Check that there are the correct number of points
+        local square_raster = primitives.square(i)
+        lu.assertEquals(square_raster:size(), i*i)
+    end
+    local test_pattern = pattern.new( {{1,1},{1,1}} )
+    lu.assertEquals(test_pattern, primitives.square(2))
+end
+-- Test line raster ------------------------------------------------
+function testPrimitives:testLine()
+    -- Draw a bunch of lines, check their properties
+    for _=1, 100, 1 do
+        local start  = cell.new(math.random(-100, 100), math.random(-100, 100))
+        local finish = cell.new(math.random(-100, 100), math.random(-100, 100))
+        local line = primitives.line( start, finish )
 
+        -- Must have start and finish cells
+        lu.assertTrue(line:has_cell(start.x,  start.y))
+        lu.assertTrue(line:has_cell(finish.x, finish.y))
+        -- Must consist of one contiguous area
+        local floodfill = subpattern.floodfill(line, start)
+        lu.assertEquals(floodfill, line)
     end
 end
