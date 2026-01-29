9817d9eab1a49838120f0390a0455b5297be225d
Nathan Hartland <nathan@pseudorandom.dev>
2026-01-29 20:58:02 +0100

    Performance enhancements in raycasting

--- forma/raycasting.lua
+++ forma/raycasting.lua
@@ -9,10 +9,41 @@
 --
 -- @module forma.raycasting
 
-local ray     = {}
+local ray = {}
 
 local cell    = require('forma.cell')
 local pattern = require('forma.pattern')
+
+-- Internal ray cast
+local function cast_xy(x0, y0, x1, y1, domain)
+    -- Start or end cell was already blocked
+    if not domain:has_cell(x0, y0) or not domain:has_cell(x1, y1) then
+        return false
+    end
+    -- Initialise line walk
+    local dx, dy = x1 - x0, y1 - y0
+    local sx = (x0 < x1) and 1 or -1
+    local sy = (y0 < y1) and 1 or -1
+    -- Rasterise step by step
+    local nx, ny = x0, y0
+    local denom = math.sqrt(dx * dx + dy * dy)
+    while nx ~= x1 or ny ~= y1 do
+        -- Ray is blocked
+        if not domain:has_cell(nx, ny) then
+            return false
+            -- Ray is not blocked, calculate next step
+        elseif math.abs(dy * (nx - x0 + sx) - dx * (ny - y0)) / denom < 0.5 then
+            nx = nx + sx
+        elseif math.abs(dy * (nx - x0) - dx * (ny - y0 + sy)) / denom < 0.5 then
+            ny = ny + sy
+        else
+            nx = nx + sx
+            ny = ny + sy
+        end
+    end
+    -- Successfully traced a ray
+    return true
+end
 
 --- Casts a ray from a start to an end cell.
 -- Returns {true/false} if the cast is successful/blocked.
@@ -27,33 +58,7 @@ function ray.cast(v0, v1, domain)
     assert(getmetatable(v0) == cell, "ray.cast requires a cell as the first argument")
     assert(getmetatable(v1) == cell, "ray.cast requires a cell as the second argument")
     assert(getmetatable(domain) == pattern, "ray.cast requires a pattern as the third argument")
-    -- Start or end cell was already blocked
-    if domain:has_cell(v0.x, v0.y) == false or domain:has_cell(v1.x, v1.y) == false then
-        return false
-    end
-    -- Initialise line walk
-    local dv = v1 - v0
-    local sx = (v0.x < v1.x) and 1 or -1
-    local sy = (v0.y < v1.y) and 1 or -1
-    -- Rasterise step by step
-    local nx = v0:clone()
-    local denom = cell.euclidean(v1, v0)
-    while (nx.x ~= v1.x or nx.y ~= v1.y) do
-        -- Ray is blocked
-        if domain:has_cell(nx.x, nx.y) == false then
-            return false
-            -- Ray is not blocked, calculate next step
-        elseif (math.abs(dv.y * (nx.x - v0.x + sx) - dv.x * (nx.y - v0.y)) / denom < 0.5) then
-            nx.x = nx.x + sx
-        elseif (math.abs(dv.y * (nx.x - v0.x) - dv.x * (nx.y - v0.y + sy)) / denom < 0.5) then
-            nx.y = nx.y + sy
-        else
-            nx.x = nx.x + sx
-            nx.y = nx.y + sy
-        end
-    end
-    -- Successfully traced a ray
-    return true
+    return cast_xy(v0.x, v0.y, v1.x, v1.y, domain)
 end
 
 --- Casts rays from a start cell across an octant.
@@ -77,16 +82,17 @@ function ray.cast_octant(v0, domain, oct, ray_length)
         if oct == 7 then return -c, -r end
         if oct == 8 then return c, -r end
     end
+    local x0, y0 = v0.x, v0.y
+    local ray_length_sq = ray_length * ray_length
     local lit_pattern = pattern.new()
-    for row = 1, ray_length, 1 do
-        for col = 0, row, 1 do
+    for row = 1, ray_length do
+        for col = 0, row do
             local tcol, trow = transformOctant(row, col)
-            local v1 = v0:clone() + cell.new(tcol, -trow)
-            if cell.euclidean2(v0, v1) < ray_length * ray_length then
-                local ray_status = ray.cast(v0, v1, domain)
-                -- Successful ray casting, add to the illuminated pattern
-                if ray_status == true then
-                    lit_pattern:insert(v1.x, v1.y)
+            local x1, y1 = x0 + tcol, y0 - trow
+            local dx, dy = x1 - x0, y1 - y0
+            if dx * dx + dy * dy < ray_length_sq then
+                if cast_xy(x0, y0, x1, y1, domain) then
+                    lit_pattern:insert(x1, y1)
                 end
             end
         end
